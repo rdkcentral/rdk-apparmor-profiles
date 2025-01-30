ae365116dddb57ac68032151f88332c45c88e2a4
Stephen Barrett <stephen_barrett@comcast.com>
2025-01-30 15:20:13 +0000

    Import of source (develop)
    Source: https://github.com/rdk-e/rdk-apparmor-profiles
    SHA1: 1.0.1

---
+++ apparmor_parse.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2024 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+Apparmor_defaults="/etc/apparmor/apparmor_defaults"
+
+PROFILES_DIR="/etc/apparmor.d/"
+PARSER="/sbin/apparmor_parser"
+SYSFS_AA_PATH="/sys/kernel/security/apparmor/profiles"
+RDKLOGS="/opt/logs/startup_stdout_log.txt"
+
+if [ -f /lib/rdk/apparmor_utils.sh ]; then
+    source /lib/rdk/apparmor_utils.sh
+fi
+
+if [ -f /lib/rdk/t2Shared_api.sh ]; then
+    source /lib/rdk/t2Shared_api.sh
+fi
+
+if [ ! -f $Apparmor_blocklist ]; then
+    touch $Apparmor_blocklist
+fi
+
+while read line; do
+        mode=`echo $line | cut -d ":" -f 2`
+        process=`echo $line | cut -d ":" -f 1`
+        profile=`ls -ltr $PROFILES_DIR | grep -w $process | awk '{print $9}'`
+        if [ ! -z $profile ]; then
+	     blocklist_process=`grep -w $process $Apparmor_blocklist`
+	     blocklist_mode=`echo $blocklist_process | awk -F ":" '{print $2}'`
+	     if [ "$mode" == "enforce" ]; then
+                     if [ "$blocklist_mode" == "complain" ]; then
+                             $PARSER -rWC $PROFILES_DIR/$profile
+                     elif [ "$blocklist_mode" != "disable" ]; then
+                             $PARSER -rW $PROFILES_DIR/$profile
+                     fi
+             elif [ "$mode" == "complain" ] && [ "$blocklist_mode" != "disable" ]; then
+                     $PARSER -rWC $PROFILES_DIR/$profile
+             fi
+        fi
+done<$Apparmor_defaults
+
+if type systemd_apparmor; then
+    systemd_apparmor
+fi
+
+list=`cat $SYSFS_AA_PATH | grep complain | awk '{print $1}' | tr '\n'  ','`
+cnt=`cat $SYSFS_AA_PATH | grep complain | wc -l`
+if [ ! -z "$list" ]; then
+     echo "List of profiles in Apparmor-complain mode:$cnt : $list" >> $RDKLOGS
+     t2ValNotify "APPARMOR_C_split:" "$cnt,$list"
+fi
+
+list=`cat $SYSFS_AA_PATH | grep enforce | awk '{print $1}' | tr '\n'  ','`
+cnt=`cat $SYSFS_AA_PATH | grep enforce | wc -l`
+if [ ! -z "$list" ]; then
+     echo "List of profiles in Apparmor-enforce mode:$cnt : $list"  >> $RDKLOGS
+     t2ValNotify "APPARMOR_E_split:" "$cnt,$list"
+fi
+
+if type apparmor_telemetry; then
+    apparmor_telemetry
+fi
